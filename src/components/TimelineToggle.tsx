7b6e1ae482a086543443d55d8d184bf271ea3013
matheusnb99 <matheusintelectual123@hotmail.com>
2025-08-11 03:49:00 +0200

    fix button

--- src/components/TimelineToggle.tsx
+++ src/components/TimelineToggle.tsx
@@ -10,11 +10,20 @@ const TimelineToggle: FunctionComponent = () => {
     "view",
     parseAsString.withDefault("simple").withOptions({ shallow: false }),
   )
+  const [, setCategory] = useQueryState(
+    "category",
+    parseAsString.withDefault("all").withOptions({ shallow: false }),
+  )
+
+  const handleChange = () => {
+    setView(handleClick)
+    setCategory("all")
+  }
 
   return (
     <button
       className="text-sm text-gray-500 hover:text-gray-700"
-      onClick={() => setView(handleClick)}
+      onClick={handleChange}
     >
       {view === "simple" ? "See detailed timeline" : "See simple timeline"}
     </button>
