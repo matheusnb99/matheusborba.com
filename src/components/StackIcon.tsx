feba8594c5a1358102331aa923d1dc125a47423e
matheusnb99 <matheusintelectual123@hotmail.com>
2025-08-12 14:30:06 +0200

    fix tech icons

--- src/components/StackIcon.tsx
+++ src/components/StackIcon.tsx
@@ -1,5 +1,9 @@
 "use client"
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 import { TECH_STACK_ICONS, TECH_STACK_LINKS } from "@/lib/constants/timeline"
 import { StackOptions } from "@/lib/types/Timeline"
 import Link from "next/link"
@@ -10,15 +14,15 @@ type Props = {
 }
 
 const StackIcon: FunctionComponent<Props> = ({ item }) => {
-  if (TECH_STACK_ICONS[item as keyof typeof TECH_STACK_ICONS]) {
+  if (!TECH_STACK_ICONS[item as keyof typeof TECH_STACK_ICONS]) {
     return (
       <span className="px-2 py-2 bg-gray-100 rounded-full text-xs">
-          <Link
-              href={TECH_STACK_LINKS[item as keyof typeof TECH_STACK_ICONS]}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="underline" 
-            >
+        <Link
+          href={TECH_STACK_LINKS[item as keyof typeof TECH_STACK_ICONS]}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline"
+        >
           {item}
         </Link>
       </span>
@@ -26,25 +30,19 @@ const StackIcon: FunctionComponent<Props> = ({ item }) => {
   }
 
   return (
-    <div
-      className="relative flex items-center justify-center p-2"
-    >
-
-
+    <div className="relative flex items-center justify-center p-2">
       <Tooltip>
         <TooltipTrigger>
           <Link
-              href={TECH_STACK_LINKS[item as keyof typeof TECH_STACK_ICONS]}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="underline" 
-            >
-              {TECH_STACK_ICONS[item as keyof typeof TECH_STACK_ICONS]}
+            href={TECH_STACK_LINKS[item as keyof typeof TECH_STACK_ICONS]}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline"
+          >
+            {TECH_STACK_ICONS[item as keyof typeof TECH_STACK_ICONS]}
           </Link>
         </TooltipTrigger>
-          <TooltipContent>
-            {item}
-        </TooltipContent>
+        <TooltipContent>{item}</TooltipContent>
       </Tooltip>
     </div>
   )
