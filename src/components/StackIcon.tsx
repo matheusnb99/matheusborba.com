cfe900f2a28dfd0c76aba69c7c89f611e41ffad7
matheusnb99 <matheusintelectual123@hotmail.com>
2025-08-12 02:45:38 +0200

    fix build errors

--- src/components/StackIcon.tsx
+++ src/components/StackIcon.tsx
@@ -3,24 +3,22 @@ import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip
 import { TECH_STACK_ICONS, TECH_STACK_LINKS } from "@/lib/constants/timeline"
 import { StackOptions } from "@/lib/types/Timeline"
 import Link from "next/link"
-import { FunctionComponent, useState } from "react"
+import { FunctionComponent } from "react"
 
 type Props = {
   item: StackOptions
 }
 
 const StackIcon: FunctionComponent<Props> = ({ item }) => {
-  const [hovered, setHovered] = useState(false)
-
-  if (!TECH_STACK_ICONS[item]) {
+  if (TECH_STACK_ICONS[item as keyof typeof TECH_STACK_ICONS]) {
     return (
       <span className="px-2 py-2 bg-gray-100 rounded-full text-xs">
-        <Link
-          href={TECH_STACK_LINKS[item]}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline"
-        >
+          <Link
+              href={TECH_STACK_LINKS[item as keyof typeof TECH_STACK_ICONS]}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline" 
+            >
           {item}
         </Link>
       </span>
