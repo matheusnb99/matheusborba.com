747b628507ee5691a957eacf12371e3b218932ca
matheusnb99 <matheusintelectual123@hotmail.com>
2025-08-26 15:26:08 +0200

    feat: add the possibility to change the side of the card

--- src/components/timeline/TimelineItem.tsx
+++ src/components/timeline/TimelineItem.tsx
@@ -8,19 +8,21 @@ import { FunctionComponent } from "react"
 type TimeLineItemProps = {
   element: TimeLineItemType
   showIcon?: boolean
+  iconPosition?: "left" | "right"
 }
 
 const TimeLineItem: FunctionComponent<TimeLineItemProps> = ({
   element,
   showIcon = true,
+  iconPosition = "left",
 }) => (
   <MotionArticle
     layout
     className={`flex gap-4 pb-10 relative ${
       showIcon
-        ? "before:absolute before:left-[17px] before:h-full before:w-0.5 before:bg-gray-200"
+        ? `before:absolute before:${iconPosition}-[17px] before:h-full before:w-0.5 before:bg-gray-200`
         : " h-full "
-    }`}
+    } ${iconPosition === "right" ? "flex-row-reverse" : ""}`}
     transition={{ layout: { type: "spring", stiffness: 400, damping: 40 } }}
   >
     {showIcon && <TimelineIcon category={element.category} />}
