f1dbff7941eaa86e66bc7a76a8ae12f762ca07bb
matheusnb99 <matheusintelectual123@hotmail.com>
2025-08-26 12:37:41 +0200

    refactor: changed key to item id

--- src/components/LastProjects.tsx
+++ src/components/LastProjects.tsx
@@ -10,7 +10,7 @@ const LastProjects: FunctionComponent<LastProjectsProps> = ({ elements }) => (
   <ul className="flex flex-col xl:flex-row gap-4 my-5 p-2 items-stretch">
     {elements.map((element) => (
       <li
-        key={element.title}
+        key={element.id}
         className="flex-1 flex flex-col self-stretch min-h-0 px-4 "
       >
         <div className="flex-1 min-h-0 h-full">
